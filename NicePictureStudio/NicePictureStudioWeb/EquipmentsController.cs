6221ba9edcb998ece53b3ce17b75bca053e3dc32
tridosayods <Tridosayods.s@hotmail.com>
2014-08-12 12:24:49 +0700

    Update Database + Scaffolding Template

---
+++ NicePictureStudio/NicePictureStudioWeb/EquipmentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NicePictureStudio.App_Data;
+
+namespace NicePictureStudio
+{
+    public class EquipmentsController : Controller
+    {
+        private NicePictureStudioDBEntities db = new NicePictureStudioDBEntities();
+
+        // GET: Equipments
+        public ActionResult Index()
+        {
+            return View(db.Equipments.ToList());
+        }
+
+        // GET: Equipments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EquipmentId,EquipmentName,Type,ModelName,EquipmentDetail,Status")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipments.Add(equipment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EquipmentId,EquipmentName,Type,ModelName,EquipmentDetail,Status")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipment equipment = db.Equipments.Find(id);
+            db.Equipments.Remove(equipment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NicePictureStudio/NicePictureStudioWeb/Models/IdentityModels.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System;
+
+namespace NicePictureStudio.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int ManagerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Status { get; set; }
+        public int IdentificationNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Education { get; set; }
+        public string Specialability { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUser>().ToTable("Employee").Property(p => p.Id).HasColumnName("EmployeeId");
+            modelBuilder.Entity<ApplicationUser>().ToTable("Employee").Property(p => p.Id).HasColumnName("EmployeeId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("EmployeeRole");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("EmployeeLogin");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("EmployeeClaim");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
